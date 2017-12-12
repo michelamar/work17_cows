2bbe75997e5b2a79615382eaed72eaa9ae9a4a93
Khinshan Khan <kkhan@stuy.edu>
2017-12-12 16:56:32 -0500

    almost done

--- find.js
+++ find.js
@@ -12,23 +12,46 @@ var targetY = boxHeight / 2;
 console.log( "box height: " + boxHeight );
 console.log( "box width: " + boxWidth );
 
+console.log( "target height: " + boxHeight/2 );
+console.log( "target width: " + boxWidth/2 );
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
     var xdist = x0-x1;
     var ydist = y0-y1;
-    return ((xdist**2) + (ydist**2)) ** (1/2)
+    //console.log(xdist);
+    //console.log(xdist);
+    return Math.sqrt((xdist*xdist) + (ydist*ydist));
 };
 
 
-var findIt = function(e) {
-    var x = event.clientX;
-    var y = event.clientY;
-    var dist = (x, y, targetX, targetY);
-    //changeColor(dist%50);
+var findIt = function (e) {
+    //var event = window.event;
+    var x = e.clientX;
+    var y = e.clientY;
+    var dist = distance(x,y,targetX,targetY);
+    if(dist < 10){
+	document.write("MOOOOOOO!!!!!!!!!!!!!");
+	return;
+    }
+    console.log("DIST: " + dist);
+    console.log("X: " + x);
+    console.log("Y: " + y);
+    //console.log(x);
+    //console.log(y);
+    //console.log(dist%255);
+    var d = (dist / 5) % 255;
+    changeColor(255 - d);
+};
+
+//chooses a color and changes to it based i=on increments of distances (50 px)
+var changeColor = function( dist ){
+    var color = (`rgb(${dist}, 0, 0)`);
+    console.log(color);
+    document.body.style.backgroundColor = color;
 };
 
 /*
-your OTHER FXNS
+  your OTHER FXNS
 */
 
 box.addEventListener("mousemove", findIt);
