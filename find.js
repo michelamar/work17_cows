2215d23d0022e94fe3f573f430887b2e17459189
michelamar <mmarchini@stuy.edu>
2017-12-13 14:15:54 -0500

    added random placement

--- find.js
+++ find.js
@@ -5,8 +5,8 @@ var boxWidth = box.offsetWidth;
 
 //hardcode target as center
 //randomize later
-var targetX = boxWidth / 2;
-var targetY = boxHeight / 2;
+var targetX = Math.random() * boxWidth;
+var targetY = Math.random() * boxHeight;
 
 
 console.log( "box height: " + boxHeight );
